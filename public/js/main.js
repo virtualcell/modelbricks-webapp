386fab51145d94bfc2503c62e556f90acf2d25ee
noahlb123 <noahliguoribills@gmail.com>
2021-07-09 14:24:26 -0400

    Tweaked Annotation Processing

--- public/js/main.js
+++ public/js/main.js
@@ -13,8 +13,10 @@ fetch("/json/annotations.json")
       //fix formatting for links
       link = link.replace('obo.go:', 'go/');
       link = link.replace('sbo:', 'sbo/');
-      link = link.replace('%3A', ':');
-      link = link.replace('%2F', '/');
+      link = link.replace(new RegExp('%3A', 'g'), ':');
+      link = link.replace(new RegExp('%2F', 'g'), '/');
+      link = link.replace(new RegExp('%28', 'g'), '(');
+      link = link.replace(new RegExp('%29', 'g'), ')');
       let prefixes = ['miriam:obo.go', 'miriam'];
       for (p in prefixes) {
         let pre = prefixes[p];
